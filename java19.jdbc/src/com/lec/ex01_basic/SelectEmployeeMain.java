b0c9a0623d262aaf0d5c09db8af49cd95b97144a
inwookbaek <iwbaek@gmail.com>
2023-05-04 15:52:40 +0900

    20230504

---
+++ java19.jdbc/src/com/lec/ex01_basic/SelectEmployeeMain.java
@@ -0,0 +1,12 @@
+package com.lec.ex01_basic;
+
+public class SelectEmployeeMain {
+
+	public static void main(String[] args) {
+		// 실습. select empno, ename, job from emp;
+		// 사원정보를 employee.html(table태그)출력파일로 저장
+		// src/com/lec/ex01_basic/empployees.html
+		// String data = "<table></table>";
+	}
+
+}
